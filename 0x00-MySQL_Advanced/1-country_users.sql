1798eef9aed72ebfdf929622dfb3cf697f943930
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-14 03:36:03 +0300

    task 1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- task 0
+-- creates a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT NOT NULL, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
