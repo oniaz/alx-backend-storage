67ad8fa89808043538e27a35454f2a10d34f3ca1
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-19 05:52:11 +0300

    task 12. Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" 12 """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    number_of_docs = collection.count_documents({})
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods_count = [
+        collection.count_documents({"method": method}) for method in methods]
+
+    status_check = collection.count_documents(
+        {"method": "GET", "path": "/status"})
+
+    print(f"{number_of_docs} logs")
+    print("Methods:")
+    for i in range(5):
+        print(f"\tmethod {methods[i]}: {methods_count[i]}")
+    print(f"{status_check} status check")
