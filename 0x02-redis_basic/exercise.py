093f7059f2e9333f8403688852164705b9f4af81
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-20 05:36:04 +0300

    2. Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 """Redis Basics Exercise"""
 
+import functools
 import redis
-import uuid
 from typing import Union, Optional, Callable
+import uuid
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator fucntion that replaces the original store method with a
+        wrapper function that increments a counter in a Redis database each
+        time the store method is called.
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function that replaces the original method and counts the
+            number of times it was called."""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -13,6 +29,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores input data to Redis for a randomly generated key using UUID
             and returns the key
