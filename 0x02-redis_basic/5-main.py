45a7a85e86eb82e80290cc250582a997240dde20
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-20 08:42:00 +0300

    ADD     @functools.wraps(method)

--- 0x02-redis_basic/5-main.py
+++ 0x02-redis_basic/5-main.py
@@ -3,15 +3,20 @@
 Main file
 """
 import redis
+import time
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis()
 get_page = __import__('web').get_page
 
 url = 'http://slowwly.robertomurray.co.uk'
 content = get_page(url)
-print(content)
+# print(content)
 
 count = r.get(f"count:{url}")
 print(int(count))
 
+time.sleep(10)
+
+count = r.get(f"count:{url}")
+print(count)
 # r.set(f"count:{url}", 0)
--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,12 +9,13 @@ from typing import Callable
 def count_requests(method: Callable) -> Callable:
     """Decorator that increments a temporary counter in Redis, keeping track of
     how many times each URL is requested. The counter lasts for 10 seconds"""
-    def wrapper(url):
+    @functools.wraps(method)
+    def wrapper(url: str) -> Callable:
         """Wrapper function that replaces the original method and counts the
         number of times the URL passed as argument has been requested"""
         if url:
             key = "count:" + url
-            r = redis.Redis(host='localhost', port=6379, db=0)
+            r = redis.Redis()
             r.incr(key)
             r.expire(key, 10)
         return method(url)
