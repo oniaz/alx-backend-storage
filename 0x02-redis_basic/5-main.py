d1f990e891af634da74de00cb506de11ac389fc5
oniaz <32391049+oniaz@users.noreply.github.com>
2024-08-10 03:00:52 +0300

    test file

---
+++ 0x02-redis_basic/5-main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import redis
+import time
+
+r = redis.Redis()
+get_page = __import__('web').get_page
+
+url = 'http://slowwly.robertomurray.co.uk'
+content = get_page(url)
+# print(content)
+
+count = r.get(f"count:{url}")
+print(count.decode()) if count else print('None')
+print(type(count))
+
+content = get_page(url)
+count = r.get(f"count:{url}")
+print(count.decode()) if count else print('None')
+print(type(count))
+
+ttl = r.ttl(f"count:{url}")
+print(ttl)
+time.sleep(10)
+
+count = r.get(f"count:{url}")
+print(count.decode()) if count else print('None')
+print(type(count))
+ttl = r.ttl(f"count:{url}")
+print(ttl)
+
+# r.set(f"count:{url}", 0)
